089f658a0242166ff156bb39723641f1a4137693
psalv <psalvato@uwo.ca>
2017-10-18 15:33:50 -0400

    Added index.

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+
+
+<body>
+
+<?php
+$page = "index-page";
+include "hexagons.php";
+?>
+
+<script src="jquery.min.js" type="text/javascript"></script>
+<script src="hexagon.js" type="text/javascript"></script>
+
+</body>
+</html>
